29a852f34e25d6092436f589cbf5ba50f8526d3a
honeycombi <garyhu06@gmail.com>
2021-05-24 13:43:17 -0400

    global site tag

---
+++ index/viewtracker.js
@@ -0,0 +1,7 @@
+async src="https://www.googletagmanager.com/gtag/js?id=G-L7NGCZSXXH">
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-L7NGCZSXXH');
